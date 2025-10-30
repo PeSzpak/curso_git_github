3248c3ab268c51b2437f741c396d5e644dd02c6d
PeSzpak <dropszpalves@gmail.com>
2025-10-30 10:40:54 -0300

    add the classe of pessoa and emprego

---
+++ classe.ts
@@ -0,0 +1,11 @@
+class Pessoa {
+    nome: string;
+    idade: number;
+    cpf: number;
+}
+class Emprego extends Pessoa {
+    empresa: string;
+    cargo: string;
+    salario: number;
+    cargaHoraria: number;
+}
